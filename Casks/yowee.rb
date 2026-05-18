7623e1756dca83a8ee97a94eea62f1fc501af399
Csaba Tamas <6283549+thingsplode@users.noreply.github.com>
2026-05-18 23:36:59 +0200

    yowee v1.2.1

--- Casks/yowee.rb
+++ Casks/yowee.rb
@@ -1,6 +1,6 @@
 cask "yowee" do
-  version "1.2.0"
-  sha256 "0ee620615e4c45b5b7ae6aa8385116247b92b41888c9c94ad32c07fbdacdcc2a"
+  version "1.2.1"
+  sha256 "4750ded91c47233ef1d8601065d4433b02ed5ec86ddecaa810f7e7671868a5ef"
 
   url "https://github.com/thingsplode/yowee/releases/download/v#{version}/yowee-#{version}.dmg"
   name "yowee"
